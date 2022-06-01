a4cb3ae773d4cf3ac0844dc2d90f87ed751da6ec
Carter Costic <cartercostic@gmail.com>
2022-06-01 12:17:47 -0400

    added project model

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,6 +1,7 @@
 from flaskr import db
 
 class Student(db.Model):
+	__tablename__ = 'student'
 	id = db.Column(db.Integer, primary_key=True)
 	fName = db.Column(db.String)
 	lName = db.Column(db.String)
@@ -14,8 +15,18 @@ class Student(db.Model):
 	college = db.Column(db.String)
 
 class Club(db.Model):
+	__tablename__ = 'club'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String)
 	description = db.Column(db.String)
 	website = db.Column(db.String)
 	images = db.Column(db.String)
+
+class Project(db.Model):
+	__tablename__ = 'project'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	description = db.Column(db.String)
+	members = db.Column(db.String)
+	source = db.Column(db.String)
+	video_url = db.Column(db.String)
